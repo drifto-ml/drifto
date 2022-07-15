cbf6148212c8e7e7d409bfe55061dc17ec216d24
tginart <tginart@gmail.com>
2022-07-15 00:54:20 -0700

    add setup.cfg and update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name='drifto',
-    version='0.1.0a',
-    description='Automatic featurization and autoML for event analytics',
+    version='0.1.0',
+    license='ELv2',
+    description='Automatic featurization and ML for event analytics',
     author='Drifto Technologies Inc',
     packages=['drifto', 'drifto.ml'],
+    url = 'https://github.com/drifto-ml/drifto',
+    keywords = ['data', 'machine learning', 'feature engineering', 'analytics', 'autoML'],
     install_requires=['pyarrow',
                       'duckdb',
                       'torch',
