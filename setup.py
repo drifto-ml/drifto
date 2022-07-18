dd9f4a851738cbc6730876132e7facabdd480e6b
tginart <tginart@gmail.com>
2022-07-18 14:34:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='drifto',
-    version='0.1.2',
+    version='0.1.3',
     license='ELv2',
     description='Automatic featurization and ML for event analytics',
     author='Drifto Technologies Inc',
